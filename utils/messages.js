da63b09ccf5fc55d6fbd51dc734724f3a81284e8
Mehedi Islam Ripon <mehediislamripon@gmail.com>
2021-05-13 16:52:07 +0600

    done and ready to deploy!

---
+++ utils/messages.js
@@ -0,0 +1,12 @@
+// dependencies
+const moment = require('moment');
+
+function formatMessage(username, text) {
+   return {
+      username,
+      text,
+      time: moment().format('h:mm a'),
+   };
+}
+
+module.exports = formatMessage;
